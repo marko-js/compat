7ecadaa230a11985758e0bce021b2fe20c31856d
Dylan Piercey <dpiercey@ebay.com>
2023-07-13 14:42:25 -0700

    chore: improve macro-body errors and tests

--- packages/compat-v4/src/components/macro-body/migrate.ts
+++ packages/compat-v4/src/components/macro-body/migrate.ts
@@ -4,30 +4,42 @@ import { diagnosticError } from "@marko/babel-utils";
 export default {
   enter(tag: t.NodePath<t.MarkoTag>) {
     const closestMacro = tag.findParent(isMacroDefinition);
+    let hasErrors = false;
 
     if (!closestMacro) {
       diagnosticError(tag, {
         label: "The <macro-body> tag can only be used inside a <macro> tag.",
       });
-      return;
+      hasErrors = true;
     }
 
     if (tag.node.attributes.length) {
       diagnosticError(tag, {
         label: "The <macro-body> tag does not support attributes.",
+        loc: tag.node.attributes[0].loc || tag.node.loc || false,
       });
+      hasErrors = true;
     }
 
     if (tag.node.body.params.length) {
       diagnosticError(tag, {
         label: "The <macro-body> tag does not support params.",
+        loc: tag.node.body.params[0].loc || tag.node.loc || false,
       });
+      hasErrors = true;
     }
 
     if (tag.node.body.body.length) {
       diagnosticError(tag, {
         label: "The <macro-body> tag does not support children.",
+        loc: tag.node.body.body[0].loc || tag.node.loc || false,
       });
+      hasErrors = true;
+    }
+
+    if (hasErrors) {
+      tag.remove();
+      return;
     }
 
     tag.replaceWith(
