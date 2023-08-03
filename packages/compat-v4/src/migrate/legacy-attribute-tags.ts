bb04236964712725b01b85da387e55a18307fe09
Dylan Piercey <dpiercey@ebay.com>
2023-08-03 11:51:23 -0700

    fix: validate nesting for legacy attribute tags before renaming

--- packages/compat-v4/src/migrate/legacy-attribute-tags.ts
+++ packages/compat-v4/src/migrate/legacy-attribute-tags.ts
@@ -1,32 +1,56 @@
 import { types as t } from "@marko/compiler";
-import { diagnosticDeprecate, withLoc } from "@marko/babel-utils";
-const legacyNestedTagSyntax = /:.*$/;
+import {
+  diagnosticDeprecate,
+  findParentTag,
+  withLoc,
+} from "@marko/babel-utils";
 
 export default {
   MarkoTag(tag) {
     const { node } = tag;
     const match =
-      node.name.type === "StringLiteral" &&
-      node.name.value.match(legacyNestedTagSyntax);
-    if (match) {
+      node.name.type === "StringLiteral" && node.name.value.split(":");
+    if (match && match.length > 1) {
+      const attrTagName = match.pop()!;
+      let curTag: t.NodePath<t.MarkoTag> | undefined = tag;
+
+      for (let i = match.length; --i > 0; ) {
+        if (
+          !(curTag = findParentTag(curTag!)) ||
+          curTag.node.name.type !== "StringLiteral" ||
+          curTag.node.name.value !== `@${match[i]}`
+        ) {
+          return;
+        }
+      }
+
+      const parentTag = findParentTag(curTag!);
+      if (
+        !parentTag ||
+        parentTag.node.name.type !== "StringLiteral" ||
+        parentTag.node.name.value !== match[0]
+      ) {
+        return;
+      }
+
       diagnosticDeprecate(tag.get("name"), {
         label:
           'The "<my-tag:nested>" tagName syntax is deprecated. Please use the "<@nested>" tagName syntax instead. See: https://github.com/marko-js/marko/wiki/Deprecation:-legacy-nested-tags',
         fix() {
-          const newName = t.stringLiteral(`@${match[0].slice(1)}`);
+          const attrTagNameLiteral = t.stringLiteral(`@${attrTagName}`);
 
           if (node.name.start != null && node.name.end != null) {
             withLoc(
               tag.hub.file,
-              newName,
-              node.name.start + match.index!,
+              attrTagNameLiteral,
+              node.name.end - attrTagName.length + 1,
               node.name.end,
             );
           }
 
           tag.replaceWith(
             t.markoTag(
-              newName,
+              attrTagNameLiteral,
               node.attributes,
               node.body,
               node.arguments,
